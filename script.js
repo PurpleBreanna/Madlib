3806420cd66f4b7c1f883a2db981c9629cc151ce
DePaul <CONF0579@depaul.edu>
2018-07-24 15:29:51 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+$( document ).ready(function() {
+	
+	
+	console.log("Hi im here");
+	
+	$( "button" ).click(function() {
+       	console.log( "clicked" );
+		
+		var noun = "dog";
+		var adjective = "big";
+		var name = "Breanna";		  
+		 var sentence1 = "There once was a " + noun; 
+		 sentence2 += '  who really wanted to find a  ' + adjective; 
+		 sentence3 += ' human ' + name + '.'; 
+		 console.log( sentence)
+		 console.log(noun);
+		 console.log(adjective);
+		 console.log(name);
+	
+	
+	
+	
+
+    }); 
+	
+	
+	
+	
+	
+	
+	
+});
