d3c81ad7bf1ab0298f461c9b10c5bf7345c10b30
DePaul <CONF0579@depaul.edu>
2018-07-25 11:25:39 -0500

    hi

--- script.js
+++ script.js
@@ -6,16 +6,18 @@ $( document ).ready(function() {
 	$( "button" ).click(function() {
        	console.log( "clicked" );
 		
-		var noun = "dog";
-		var adjective = "big";
-		var name = "Breanna";		  
-		 var sentence1 = "There once was a " + noun; 
-		 sentence2 += '  who really wanted to find a  ' + adjective; 
-		 sentence3 += ' human ' + name + '.'; 
-		 console.log( sentence)
+		var noun = $("#noun").val();
+		var adjective = $("#adjective").val();
+		var name = $("#name").val();		  
+		var sentence1 = "There once was a " + noun; 
+		var sentence2 = '  who really wanted to find a  ' + adjective; 
+		 var sentence3 = ' human ' + name + '.'; 
+		 console.log( sentence1)
 		 console.log(noun);
 		 console.log(adjective);
 		 console.log(name);
+		 
+		 $("#result").html( "There once was a " + noun +'  who really wanted to find a  ' + adjective +' human ' + name + '!');
 	
 	
 	
